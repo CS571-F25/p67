01ca2ad76b6d3c413cdd6095c1819e5d91cd889e
Elden Antonio <eldengantonio@gmail.com>
2025-11-17 19:32:36 -0600

    added logos

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,13 +1,19 @@
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Image } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../assets/Logo_Icon_Antonyx_Consulting.png';
 
 export default function TopBar() {
   return (
     <Navbar bg="light" expand="lg" fixed="top" className="border-bottom shadow-sm">
       <Container>
         <Navbar.Brand href="#/">
-          🏠
+          <Image 
+            src={logo} 
+            alt="Antonyx Consulting Logo" 
+            height="30" 
+            className="d-inline-block align-top"
+          />
         </Navbar.Brand>
         
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
